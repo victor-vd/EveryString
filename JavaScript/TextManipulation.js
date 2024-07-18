61a2b92426538eabb2bcd5bce775740c263a41df
ohtomu <150762200+ohtomu@users.noreply.github.com>
2024-07-18 17:34:18 -0300

    text capitalization finished and some upgrades in the style

--- JavaScript/TextManipulation.js
+++ JavaScript/TextManipulation.js
@@ -1,15 +1,11 @@
-//const TEXT_BOX_INPUT = document.getElementById("textBoxInput");
+const TEXT_BOX_INPUT = document.getElementById("textBoxInput");
 const TEXT_BOX_OUTPUT = document.getElementById("textBoxOutput");
 //const SUBMIT_BUTTON = document.getElementById("submitButton");const TEXT_BOX_INPUT = document.getElementById("textBoxInput");
 let userText = "";
 let outputText = "";
-let toLowerCase = false;
-let toUpperCase = false;
-let toFirstLetter = false;
-let toParagraph = false;
 
 function buttonSubmitClick(){
-    console.log(Check.toLowerCase, Check.toUpperCase, Check.toFirstLetter, Check.toParagraph)
+    console.log(Check.getToLowerCase, Check.getToUpperCase, Check.getToFirstLetter)
 }
 
 const updateTextInput = {
@@ -21,49 +17,45 @@ const updateTextInput = {
         TEXT_BOX_OUTPUT.value = textCaptilizer(textBoxInput);
     }
 }
-
 const Check = {
-    toLowerCase: false,
-    toUpperCase: false,
-    toFirstLetter: false,
-    toParagraph: false,
+    getToLowerCase: false,
+    getToUpperCase: false,
+    getToFirstLetter: false,
     checkThis: function verifyChecked(name){
         if(name == 'checkboxCaps'){
-            Check.toLowerCase = document.querySelector(`input[id="toLowerCase"`).checked;
-            Check.toUpperCase = document.querySelector(`input[id="toUpperCase"`).checked;
-            Check.toFirstLetter = document.querySelector(`input[id="toFirstLetter"`).checked;
-            Check.toParagraph = document.querySelector(`input[id="toParagraph"`).checked;
+            Check.getToLowerCase = document.querySelector(`input[id="getToLowerCase"`).checked;
+            Check.getToUpperCase = document.querySelector(`input[id="getToUpperCase"`).checked;
+            Check.getToFirstLetter = document.querySelector(`input[id="getToFirstLetter"`).checked;
+            TEXT_BOX_OUTPUT.value = textCaptilizer(TEXT_BOX_INPUT.value)
         }
     }
 }
 
-function textCaptilizer(textBoxInput = ""){
-    if(Check.toLowerCase){
+function textCaptilizer(textBoxInput){
+    if(Check.getToLowerCase){
         textBoxInput = textBoxInput.toLowerCase();
-    } else if (Check.toUpperCase) {
+    } else if (Check.getToUpperCase) {
         textBoxInput = textBoxInput.toUpperCase();
-    } else if (Check.toFirstLetter) {
-        textBoxInput = function(){
-            textBoxInput = textBoxInput.trim().toLowerCase();
-            let wordNumber = 1;
-            let strings = textBoxInput.split(' ');
-        
-            for(let i=0; i<=textBoxInput.length; i++){
-                if(textBoxInput.charAt(i)==' '){
-                    wordNumber++;
-                }
-            }
-            textBoxInput = '';
-            for(let i=0; i<wordNumber; i++){
-                strings[i] = strings[i].charAt(0).toUpperCase()+strings[i].slice(1);
-                textBoxInput += i==wordNumber-1? strings[i]: strings[i] + ' ';
-            }
-            return textBoxInput;
-        }
-    } else if (Check.toParagraph) {
-        textBoxInput = function(){
+    } else if (Check.getToFirstLetter) {
+        textBoxInput = toFirstLetter(textBoxInput);
+    }
+    return textBoxInput;
+}
 
+function toFirstLetter(textBoxInput){
+    textBoxInput = textBoxInput.trim().toLowerCase();
+    let wordNumber = 1;
+    let strings = textBoxInput.split(' ');
+
+    for(let i=0; i<=textBoxInput.length; i++){
+        if(textBoxInput.charAt(i)==' '){
+            wordNumber++;
         }
+    }
+    textBoxInput = '';
+    for(let i=0; i<wordNumber; i++){
+        strings[i] = strings[i].charAt(0).toUpperCase()+strings[i].slice(1);
+        textBoxInput += i==wordNumber-1? strings[i]: strings[i] + ' ';
     }
     return textBoxInput;
 }
@@ -76,6 +68,7 @@ function textInverter(textBoxInput){
     }   
     return outputText;
 }
+
 /*
 class Check{
     constructor(name){
