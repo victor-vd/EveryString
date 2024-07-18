83cf0be5380e2b4b77b34c28876520aa4007cf39
ohtomu <150762200+ohtomu@users.noreply.github.com>
2024-07-18 17:57:43 -0300

    home page fixed

--- JavaScript/ElementInject.js
+++ JavaScript/ElementInject.js
@@ -1,7 +1,7 @@
 document.querySelector("nav").innerHTML = `
 <a href=""><img src="../Assets/Images/pixelArtPNG.png" alt=""></a>
 <div class="links">
-    <a href="">HOME</a>
+    <a href="../HomePage.html">HOME</a>
     <a href="">SOCIAL</a>
     <a href="">GITHUB</a>
 </div>`
