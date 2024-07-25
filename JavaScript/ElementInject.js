cefa0fa30d712d06aec7b3ed21eadde4d0778721
ohtomu <150762200+ohtomu@users.noreply.github.com>
2024-07-25 04:21:53 -0300

    code modified

--- JavaScript/ElementInject.js
+++ JavaScript/ElementInject.js
@@ -2,6 +2,6 @@ document.querySelector("nav").innerHTML = `
 <a href=""><img src="../Assets/Images/pixelArtPNG.png" alt=""></a>
 <div class="links">
     <a href="HomePage.html">HOME</a>
-    <a href="ig.com/victor_vsdd">SOCIAL</a>
-    <a href="github.com/victor-vd">GITHUB</a>
+    <a href="https://www.instagram.com/victor_vsdd/">SOCIAL</a>
+    <a href="https://www.github.com/victor-vd">GITHUB</a>
 </div>`
