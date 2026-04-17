68b3748fdf8b1d266a65f9044a5c44654eac6f0b
victor-vd <victorsdd80@gmail.com>
2026-04-17 03:17:19 -0300

    carterinha

--- JavaScript/TextManipulation.js
+++ JavaScript/TextManipulation.js
@@ -1,12 +1,8 @@
 const TEXT_BOX_INPUT = document.getElementById("textBoxInput");
 const TEXT_BOX_OUTPUT = document.getElementById("textBoxOutput");
-//const SUBMIT_BUTTON = document.getElementById("submitButton");const TEXT_BOX_INPUT = document.getElementById("textBoxInput");
+// const SUBMIT_BUTTON = document.getElementById("submitButton");const TEXT_BOX_INPUT = document.getElementById("textBoxInput");
 let userText = "";
 let outputText = "";
-
-function buttonSubmitClick(){
-    console.log(Check.getToLowerCase, Check.getToUpperCase, Check.getToFirstLetter, Check.getToLowerCase)
-}
 
 const updateTextInput = {
     getInvertedText: function(textBoxInput){
@@ -25,10 +21,10 @@ const Check = {
     
     checkThis: function verifyChecked(name){
         if(name == 'checkboxCaps'){
-            Check.getToLowerCase = document.querySelector(`input[id="getToLowerCase"`).checked;
-            Check.getCarteirinha = document.querySelector(`input[id="getCarteirinha"`).checked;
-            Check.getToUpperCase = document.querySelector(`input[id="getToUpperCase"`).checked;
-            Check.getToFirstLetter = document.querySelector(`input[id="getToFirstLetter"`).checked;
+            Check.getToLowerCase = document.querySelector(`input[id="getToLowerCase"]`).checked;
+            Check.getCarteirinha = document.querySelector(`input[id="getCarteirinha"]`).checked;
+            Check.getToUpperCase = document.querySelector(`input[id="getToUpperCase"]`).checked;
+            Check.getToFirstLetter = document.querySelector(`input[id="getToFirstLetter"]`).checked;
             TEXT_BOX_OUTPUT.value = textCaptilizer(TEXT_BOX_INPUT.value)
             if(!Check.getCarteirinha){
                 for (const el of document.querySelectorAll(".textBoxes textarea")) {
@@ -39,129 +35,184 @@ const Check = {
     }
 }
 
+function buttonSubmitClick(){
+    console.log(Check.getToLowerCase, Check.getToUpperCase, Check.getToFirstLetter, Check.getToLowerCase)
+}
+
 function textCaptilizer(textBoxInput){
+    if(Check.getCarteirinha){
+        document.querySelectorAll(".textBoxes textarea")
+            .forEach(el => el.classList.add("carteira"));
+
+        return carteirinha(textBoxInput);
+    }
+
+    document.querySelectorAll(".textBoxes textarea")
+        .forEach(el => el.classList.remove("carteira"));
+
     if(Check.getToLowerCase){
-        textBoxInput = textBoxInput.toLowerCase();
-    } else if (Check.getToUpperCase) {
-        textBoxInput = textBoxInput.toUpperCase();
-    } else if (Check.getToFirstLetter) {
-        textBoxInput = toFirstLetter(textBoxInput);
-    } else if (Check.getCarteirinha) {
+        return textBoxInput.toLowerCase();
+    }
+
+    if (Check.getToUpperCase) {
+        return textBoxInput.toUpperCase();
+    }
+
+    if (Check.getToFirstLetter) {
+        return toFirstLetter(textBoxInput);
+    }
+
+    return textBoxInput;
+}
+
+function extrairDadosRenv(text) {
+console.log(text);
+
+    // 1. Limpeza total para busca
+    const cleanedText = text.map(line => line.replaceAll('O', '0').replaceAll('I', '1').replaceAll('S', '5'));
+
+    const schoolId = cleanedText.find(l =>
+        /^[0-9O]{8}$/.test(l)
+    );
+    const regex = new RegExp(`(?<![A-Za-z])${schoolId}(?![A-Za-z])`, "g");    
+    const startIndex = cleanedText.findIndex(l => l.match(regex));
+
+    if (startIndex === -1) return null;
+
+    const lines = text.slice(startIndex);
+    const estudante = lines[1];
+
+    let date = null;
+
+    for (const line of lines) {
+        const normalizedLine = line
+            .replace(/[oO]/g, "0")
+            .replace(/[iI]/g, "1")
+            .replace(/[sS]/g, "5");
+
+        const matchDate = normalizedLine.match(/\d{2}\/\d{2}\/\d{4}/);
         
-        for (const el of document.querySelectorAll(".textBoxes textarea")) {
-            el.classList.add("carteira");
-        }
-        textBoxInput = carteirinha(textBoxInput);
-    }
-    return textBoxInput;
-}
-
-function toFirstLetter(textBoxInput){
-    textBoxInput = textBoxInput.trim().toLowerCase();
-    let wordNumber = 1;
-    let strings = textBoxInput.split(' ');
-
-    for(let i=0; i<=textBoxInput.length; i++){
-        if(textBoxInput.charAt(i)==' '){
-            wordNumber++;
+        if (matchDate) {
+            date = matchDate[0];
+            break; 
         }
     }
+    const names = lines.filter(isName);
+    names.length = names.length-2;
 
-    textBoxInput = '';
+    // remove duplicados
+    const uniqueNames = [...new Set(names)];
 
-    for(let i=0; i<wordNumber; i++){
-        strings[i] = strings[i].charAt(0).toUpperCase()+strings[i].slice(1);
-        textBoxInput += i==wordNumber-1? strings[i]: strings[i] + ' ';
-    }
-    return textBoxInput;
-}
+    // remove estudante
+    const filtered = uniqueNames.filter(n => n !== estudante);
 
-function corrigirNome(nome) {
-    const correcoes = {
-        'JOAO': 'JOÃO',
-        'CONCEICAO': 'CONCEIÇÃO',
-        'GONCALVES': 'GONÇALVES',
-        'PAIXAO': 'PAIXÃO',
-        'BRANDAO': 'BRANDÃO',
-        'JOSE': 'JOSÉ',
-        'SA': 'SÁ'
+    // pega os dois últimos
+    const filiacao1 = filtered.at(-1) || "";
+    const filiacao2 = filtered.at(-2) || "";
+
+    const result = {
+        estudante: normalizeName(estudante),
+        filiacao1: normalizeName(filiacao1),
+        filiacao2: normalizeName(filiacao2),
+        nascimento: date
     };
 
-    return nome.split(' ')
-        .map(p => correcoes[p] || p)
-        .join(' ');
+    return result;
+}
+
+function extrairDadosNorm(text) {
+
+    let primeiroNome = null;
+    let date = null;
+
+    for (const line of text) {
+        const matchDate = line.match(
+                /(?:Data de Nascimento|Data|Nascimento)\s*:\s*([\dIiSsOo]{1,2}\/[\dIiSsOo]{1,2}\/[\dIiSsOo]{4})/i
+            );
+        if (matchDate) {
+            date = matchDate[1]
+                .replace(/[iI]/g, "1")
+                .replace(/[oO]/g, "0")
+                .replace(/[sS]/g, "5");
+
+            const [d, m, y] = date.split('/');
+            date = `${d.padStart(2, '0')}/${m.padStart(2, '0')}/${y}`;
+        }
+    }
+
+    const nomes = text
+        .map(line => line.match(/(?:Nome|N0me|d0me|dome)\s*:\s*([A-ZÀ-Ü\s-]+)/i))
+        .filter(Boolean)
+        .map(match => match[1]);
+        
+    nomes.splice(-1, 1);
+
+
+    const estudante = nomes[0];
+
+    // remove estudante
+    const filtered = nomes.filter(n => n !== estudante);
+
+    // pega os dois últimos
+    const filiacao1 = filtered.at(-1) || "";
+    const filiacao2 = filtered.at(-2) || "";
+
+
+    const result = {
+        estudante: normalizeName(estudante),
+        filiacao1: normalizeName(filiacao1),
+        filiacao2: normalizeName(filiacao2),
+        nascimento: date
+    };
+
+    return result;
 }
 
 function carteirinha(textBoxInput) {
+    if(textBoxInput == "" || textBoxInput == null){
+        return "";
+    }
     const text = corrigirNome(textBoxInput);
-    console.log(text);
 
-    // pega todos os nomes
-    const nomes = [...text.matchAll(/(?:Nome|N0me|d0me|dome):\s*([A-ZÀ-Ü\s-]+)/gi)]
-        .map(m => {
-            let nome = m[1]
-                .replace(/-/g, '')           // remove hífen
-                .replace(/\s+/g, ' ')
-                .trim();
+    const rawLines = text
+        .split("\n")
+        .filter(Boolean);
 
-            nome = nome.split(' ')
-                .filter(p => /^[A-ZÀ-Ü]{2,}$/.test(p))
-                .join(' ');
+    const isRenovacao =
+        /RENOV|NO: 2026|NO: 2026|MUNICIPAL|RENOVA|RENOVAG|RENOVAÇÃO|NO:/i.test(text);
 
-            return nome;
-        });
+    console.log("isRenovacao: ", isRenovacao);
 
-    // data de nascimento (mais robusto)
-    const nascimentoMatch = text.match(
-        /(?:Data de Nascimento|Data|Nascimento):\s*([\dIiSsOo]{1,2}\/[\dIiSsOo]{1,2}\/[\dIiSsOo]{4})/i
-    );
+    let data;
 
-    let nascimento = null;
-
-    if (nascimentoMatch) {
-    nascimento = nascimentoMatch[1]
-        .replace(/[iI]/g, "1")
-        .replace(/[oO]/g, "0")
-        .replace(/[sS]/g, "5");
-
-        // opcional: padronizar (ex: 5/1/2013 → 05/01/2013)
-        const [d, m, y] = nascimento.split('/');
-        nascimento = `${d.padStart(2, '0')}/${m.padStart(2, '0')}/${y}`;
+    if (isRenovacao) {
+        data = extrairDadosRenv(rawLines);
+    } else {
+        data = extrairDadosNorm(rawLines);
     }
-    const nomesUnicos = [];
 
-    for (const nome of nomes) {
-        const normalizado = nome
-        .replace(/[^A-ZÀ-Ü\s]/gi, '')
-        .replace(/\s+/g, ' ')
-        .trim()
-        .toUpperCase();
+    const clean = (str) => {
+        if (!str) return "";
 
-    if (!nomesUnicos.some(n =>
-        n.replace(/[^A-ZÀ-Ü\s]/gi, '').replace(/\s+/g, ' ').trim().toUpperCase() === normalizado
-    )) {
-        nomesUnicos.push(nome);
-    }
+        const normalized = str
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "") // remove acento
+            .toLowerCase();
+
+        if (/sem\s+[o0]bserva[cg][o0]es|nao\s+consta/.test(normalized)) {
+            return "";
+        }
+
+        return str.trim();
+    };
+    
+    return [
+        clean(data?.estudante),
+        clean(data?.filiacao2),
+        clean(data?.filiacao1),
+        clean(data?.nascimento)
+    ].join("\n");
 }
-
-const estudante = nomesUnicos[0] || "";
-const pais = nomesUnicos.slice(1, 3);
-
-const data = {
-    estudante: toFirstLetter(estudante),
-    filiacao1: toFirstLetter(pais[0] || ""),
-    filiacao2: toFirstLetter(pais[1] || ""),
-    nascimento
-};
-
-    console.log(data);
-
-    return `${data.estudante}
-${data.filiacao1}
-${data.filiacao2}
-${data.nascimento}`;
-}
-
 
 function textInverter(textBoxInput){
     outputText = "";
@@ -170,4 +221,80 @@ function textInverter(textBoxInput){
         outputText = c + outputText;
     }   
     return outputText;
+}
+
+function toFirstLetter(text) {
+    return text
+        .toLowerCase()
+        .split(' ')
+        .map(p => p.charAt(0).toUpperCase() + p.slice(1))
+        .join(' ');
+}
+
+function corrigirNome(text) {
+    const correcoes = {
+        'JOAO': 'JOÃO',
+        'JOÄO': 'JOÃO',
+        'CONCEICAO': 'CONCEIÇÃO',
+        'CONCEICÃO': 'CONCEIÇÃO',
+        'CONCEICÄO': 'CONCEIÇÃO',
+        'CONCEIÇÄO': 'CONCEIÇÃO',
+        'CONCEIÇAO': 'CONCEIÇÃO',
+        'GONCALVES': 'GONÇALVES',
+        'PAIXAO': 'PAIXÃO',
+        'BRANDAO': 'BRANDÃO',
+        'JOSE': 'JOSÉ',
+        'UMA': 'LIMA',
+        'ALMIRANOA': 'ALMIRANDA',
+        'SCARES': 'SOARES',
+        'SA': 'SÁ'
+    };
+
+    const resultado = text
+        .toUpperCase()
+        .replace(/0/g, 'O')
+        .replace(/OOS/g, 'DOS')
+        .replace(/A0/g, 'AO')
+        .replace(/I0/g, 'IO')
+        .replace(/N0ME|D0ME/g, 'NOME')
+        .replace(/AO\b/g, 'ÃO')
+        .replace(/CAO\b/g, 'ÇÃO')
+        .replace(/OO/g, 'Do')
+        .replace(/OA/g, 'Da')
+        .replace(/OAS/g, 'Das')
+        .replace(/\b[A-ZÀ-Ü]+\b/g, (palavra) => correcoes[palavra] || palavra);
+
+    console.log(resultado);
+    return resultado;
+}
+
+// melhora nomes tipo "MORAES / MORAE-S"
+function normalizeName(name) {
+    console.log("name.replace - ",name.replace(/-/g, " "))
+    return toFirstLetter(
+        name.replace(/-/g, " ")
+    );
+}
+
+function cleanLine(line) {
+    return (line || "")
+        .replace(/\s+/g, " ")
+        .trim();
+}
+
+function isDate(line) {
+    return /\d{2}\/\d{2}\/\d{4}/.test(line);
+}
+
+function isCPF(line) {
+    return /\d{3}\.\d{3}\.\d{3}-\d{2}/.test(line);
+}
+
+function isName(line) {
+    return (
+        line &&
+        !isDate(line) &&
+        !isCPF(line) &&
+        !/\b(CEP|RUA|NO|MATRICULADO|OBSERV|MASCULINO|FEMININO|PARDA)\b/i.test(line)
+    );
 }
